eaa4fe29826e982c0c4d54b4053090d9828dcf72
AcidHardcore <vitcore@mail.ru>
2016-05-18 15:45:52 +0300

    processing a price block

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,32 +1,45 @@
 var butPrev = document.querySelector('.review__prev-js');
 var butNext = document.querySelector('.review__next-js');
 
-//create slider
-var slider = Peppermint(document.getElementById('peppermint'), {
+//create sliderReview
+var sliderReview = Peppermint(document.getElementById('peppermint'), {
     dots: true,
     slideshow: true,
     speed: 500,
     slideshowInterval: 5000,
-    stopSlideshowAfterInteraction: true,
-    onSetup: function(n) {
-        console.log('Peppermint setup done. Slides found: ' + n);
-    }
+    stopSlideshowAfterInteraction: true
+    // onSetup: function(n) {
+    //     console.log('Peppermint setup done. Slides found: ' + n);
+    // }
 });
 
-//slider prev button
+//sliderReview prev button
 butPrev.addEventListener('tap', function (event) {
     event.preventDefault();
-    slider.prev();
+    sliderReview.prev();
 
 });
 
-//slider next button
+//sliderReview next button
 butNext.addEventListener('tap', function (event) {
     event.preventDefault();
-    slider.next();
+    sliderReview.next();
 
 });
 
 /*
  butPrev.addEventListener('tap', slider.prev, false);
  butNext.addEventListener('tap', slider.next, false);*/
+
+//Create sliderPrice
+var sliderPrice = Peppermint(document.getElementById('sliderprice'), {
+    dots: true,
+    slideshow: true,
+    speed: 500,
+    slideshowInterval: 5000,
+    startSlide: 1,
+    stopSlideshowAfterInteraction: true
+    // onSetup: function(n) {
+    //     console.log('Peppermint setup done. Slides found: ' + n);
+    // }
+});
